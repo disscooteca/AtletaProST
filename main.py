9175c9243fd87dfc0d8019459970398ffadcb117
SAMUEL CARVALHO DE ALMEIDA <samuelca.senai@fieg.com.br>
2025-10-15 08:25:59 -0300

    alterações pedidas pelo rafael

--- main.py
+++ main.py
@@ -549,8 +549,8 @@ if selected == "Painel de Controle":
             'Outro': '#868E96'               # Cinza azulado
         }
 
-        st.write(db_grupo['StatusE'])
-        if db_grupo['StatusE'] == "Outro":
+        st.write(db_grupo['StatusE'][0])
+        if db_grupo['StatusE'][0] == "Outro":
             fig = px.bar(db_grupo, 
                                 y='Label_Y', 
                                 x="Quantidade Atual", 
